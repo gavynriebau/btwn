538d29a8dc189d970e70c2c4a7591525ef7134ba
Gavyn Riebau <gavyn.riebau@gmail.com>
2023-02-11 07:07:20 +0800

    Pass artifacts between jobs

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 VERSION=$(cat Cargo.toml | grep '^version = "' | cut -d' ' -f 3 | tr -d '"')
-mkdir artifacts
-cp ./target/release/btwn ./artifacts/btwn-linux-x64
-cp ./target/x86_64-pc-windows-gnu/release/btwn.exe ./artifacts/btwn-win-x64.exe
-gh release create "v$VERSION" ./artifacts/btwn-linux-x64 ./artifacts/btwn-win-x64.exe
+gh release create "v$VERSION" ./btwn-linux-x64 ./btwn-win-x64.exe
